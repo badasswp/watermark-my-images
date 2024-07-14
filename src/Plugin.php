45d12f6c56adef60394a4394ae95f121f751356f
badasswp <badasswpdev@gmail.com>
2024-07-14 20:13:06 +0100

    feat: implement method for add watermark metadata

--- src/Plugin.php
+++ src/Plugin.php
@@ -164,4 +164,29 @@ class Plugin {
 		$image->paste( $text, new Point( 0, 0 ) );
 		$image->save( $img_absolute_path );
 	}
+
+	/**
+	 * Add Watermark Meta.
+	 *
+	 * This method is responsible for capturing meta-data
+	 * if watermarking was successful.
+	 *
+	 * @param string|\WP_Error $html      Image HTML or WP_Error.
+	 * @param string[]         $watermark Watermark paths.
+	 * @param int              $id        Image ID.
+	 *
+	 * @return void
+	 */
+	public function add_watermark_metadata( $html, $watermark, $id ): void {
+		if ( ! is_wp_error( $html ) ) {
+			update_post_meta(
+				$id,
+				'watermark_my_images',
+				[
+					'abs' => $watermark['abs'] ?? '',
+					'rel' => $watermark['rel'] ?? '',
+				]
+			);
+		}
+	}
 }

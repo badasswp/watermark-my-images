ae7b2ca58231cebfd1bf09abf465812bc0f4f1e8
badasswp <badasswpdev@gmail.com>
2024-07-15 11:48:35 +0100

    feat: implement Watermarker class

---
+++ inc/Core/Watermarker.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Watermarker Class.
+ *
+ * This is responsible for all the magic goodness happening
+ * behind the scene on the plugin.
+ *
+ * @package WatermarkMyImages
+ */
+
+namespace WatermarkMyImages\Core;
+
+use WatermarkMyImages\Abstracts\Service;
+
+class Watermarker {
+	/**
+	 * Set up.
+	 *
+	 * @param Service $service
+	 */
+	public function __construct( Service $service ) {
+		$this->image_id = $service->image_id;
+	}
+
+	/**
+	 * Get Watermark.
+	 *
+	 * This method is responsible for handling custom
+	 * watermarking operations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @throws \Exception $e When unable to datect Image ID.
+	 * @throws \Exception $e When unable to create Text Drawer object.
+	 * @throws \Exception $e When unable to open Image resource.
+	 * @throws \Exception $e When unable to paste Text on Image resource.
+	 * @throws \Exception $e When unable to save Watermark Image.
+	 *
+	 * @return string[]
+	 */
+	public function get_watermark(): array {
+		$img_absolute_path = get_attached_file( $this->image_id );
+
+		if ( ! file_exists( $img_absolute_path ) ) {
+			throw new \Exception(
+				sprintf(
+					esc_html__( 'Unable to create Image watermark, file does not exist for Image ID: %d.', 'watermark-my-images' ),
+					$this->image_id
+				)
+			);
+		}
+
+		try {
+			$text = ( new Text() )->get_text();
+		} catch ( \Exception $e ) {
+			throw new \Exception(
+				sprintf(
+					esc_html__( 'Unable to create Text object, %s', 'watermark-my-images' ),
+					$e->getMessage()
+				)
+			);
+		}
+
+		try {
+			$image = ( new Imagine() )->open( $img_absolute_path );
+		} catch ( \Exception $e ) {
+			throw new \Exception(
+				sprintf(
+					esc_html__( 'Unable to open Image Resource, %s', 'watermark-my-images' ),
+					$e->getMessage()
+				)
+			);
+		}
+
+		try {
+			$image->paste( $text, new Point( 0, 0 ) );
+		} catch ( \Exception $e ) {
+			throw new \Exception(
+				sprintf(
+					esc_html__( 'Unable to paste Text on Image resource, %s', 'watermark-my-images' ),
+					$e->getMessage()
+				)
+			);
+		}
+
+		try {
+			$image->save( $this->get_watermark_abs_path() );
+		} catch ( \Exception $e ) {
+			throw new \Exception(
+				sprintf(
+					esc_html__( 'Unable to save to Watermark image, %s', 'watermark-my-images' ),
+					$e->getMessage()
+				)
+			);
+		}
+
+		return [
+			'abs' => $this->get_watermark_abs_path(),
+			'rel' => $this->get_watermark_rel_path(),
+		];
+	}
+
+	/**
+	 * Get Watermark absolute path.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_watermark_abs_path(): string {
+		$img_absolute_path = get_attached_file( $this->image_id );
+
+		return str_replace(
+			pathinfo( $img_absolute_path, PATHINFO_BASENAME ),
+			'watermark-my-images-' . $this->image_id . '.jpg',
+			$img_absolute_path
+		);
+	}
+
+	/**
+	 * Get Watermark relative path.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_watermark_rel_path(): string {
+		$url = wp_get_attachment_url( $this->image_id );
+
+		return str_replace(
+			substr( $url, strrpos( $url, '/' ) ),
+			'watermark-my-images-' . $this->image_id . '.jpg',
+			$url
+		);
+	}
+}

c3c02213aae66fdbfb5ece893d170e631cbdecaf
badasswp <badasswpdev@gmail.com>
2024-07-15 11:51:40 +0100

    chore: implement empty classes for now

---
+++ inc/Services/Admin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Admin Class.
+ *
+ * @package WatermarkMyImages
+ */
+
+namespace WatermarkMyImages\Services;
+
+class Admin extends Service implements Registrable {
+	/**
+	 * Bind to WP.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+
+	}
+}
---
+++ inc/Services/PageLoad.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PageLoad Class.
+ *
+ * @package WatermarkMyImages
+ */
+
+namespace WatermarkMyImages\Services;
+
+class PageLoad extends Service implements Registrable {
+	/**
+	 * Bind to WP.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+
+	}
+}

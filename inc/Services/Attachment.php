5cd1e37f15035bbf4628a399c53e7f16fa2b6304
badasswp <badasswpdev@gmail.com>
2024-08-11 22:55:14 +0100

    chore: add Attachment removal logic

--- inc/Services/Attachment.php
+++ inc/Services/Attachment.php
@@ -23,6 +23,7 @@ class Attachment extends Service implements Registrable {
 	 */
 	public function register(): void {
 		add_action( 'add_attachment', [ $this, 'add_watermark_on_add_attachment' ], 10, 1 );
+		add_action( 'delete_attachment', [ $this, 'remove_watermark_on_delete_attachment' ], 10, 1 );
 	}
 
 	/**
@@ -73,5 +74,41 @@ class Attachment extends Service implements Registrable {
 		 * @return void
 		 */
 		do_action( 'watermark_my_images_on_add_attachment', $response, $watermark ?? [], $id = $this->image_id );
+	}
+
+	/**
+	 * Remove Watermark Image.
+	 *
+	 * This method removes the Watermark images
+	 * associated with an Image.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $image_id Image ID.
+	 * @return void
+	 */
+	public function remove_watermark_on_delete_attachment( $image_id ): void {
+		if ( ! wp_attachment_is_image( $image_id ) ) {
+			return;
+		}
+
+		// Get absolute path for main image.
+		$main_image = get_post_meta( $image_id, 'watermark_my_images', true );
+
+		if ( file_exists( $main_image['abs'] ?? '' ) ) {
+			unlink( $main_image['abs'] );
+
+			/**
+			 * Fires after Watermark Image has been deleted.
+			 *
+			 * @since 1.0.0
+			 *
+			 * @param string $main_image['abs'] Absolute path to Watermark image.
+			 * @param int    $image_id          Image ID.
+			 *
+			 * @return void
+			 */
+			do_action( 'watermark_my_images_on_delete_attachment', $main_image['abs'], $image_id );
+		}
 	}
 }

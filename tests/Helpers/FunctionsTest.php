c6fa7d63df1890aa3123a63b04b5e15826149857
badasswp <badasswpdev@gmail.com>
2024-10-01 19:09:06 +0100

    test: add tests for get equivalents

--- tests/Helpers/FunctionsTest.php
+++ tests/Helpers/FunctionsTest.php
@@ -24,4 +24,12 @@ class FunctionsTest extends TestCase {
 
 		$this->assertTrue( $is_watermark_added_on_upload );
 	}
+
+	public function test_wmi_get_equivalent() {
+		$watermark_abs_url = wmi_get_equivalent( '/var/www/wp-content/uploads/2024/09/sample-1.png' );
+		$watermark_rel_url = wmi_get_equivalent( 'https://example.com/wp-content/uploads/2024/09/sample-2.png' );
+
+		$this->assertSame( $watermark_abs_url, '/var/www/wp-content/uploads/2024/09/sample-1-watermark-my-images.jpg' );
+		$this->assertSame( $watermark_rel_url, 'https://example.com/wp-content/uploads/2024/09/sample-2-watermark-my-images.jpg' );
+	}
 }

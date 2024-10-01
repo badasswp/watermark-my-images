09debd4f7399dbca7afd8654b0800102387349e0
badasswp <badasswpdev@gmail.com>
2024-10-01 18:25:09 +0100

    test: add test for Registrable interface

---
+++ tests/Interfaces/RegistrableTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WatermarkMyImages\Tests\Interfaces;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+use WatermarkMyImages\Interfaces\Registrable;
+
+/**
+ * @covers \WatermarkMyImages\Interfaces\Registrable::__construct
+ */
+class RegistrableTest extends TestCase {
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		$this->registrable = $this->getMockForAbstractClass( Registrable::class );
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_register() {
+		$this->registrable->expects( $this->once() )
+			->method( 'register' );
+
+		$this->registrable->register();
+
+		$this->assertConditionsMet();
+	}
+}

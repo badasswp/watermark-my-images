b6d6046f2fc6142044d70c1486b9031e1355ce8b
badasswp <badasswpdev@gmail.com>
2024-10-01 18:15:37 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/watermark-my-images',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b6d14854dc0a076cb38830167acfe8bf6eb6cfc9',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/watermark-my-images' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b6d14854dc0a076cb38830167acfe8bf6eb6cfc9',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'imagine/imagine' => array(
+            'pretty_version' => '1.3.5',
+            'version' => '1.3.5.0',
+            'reference' => '7151d553edec4dc2bbac60419f7a74ff34700e7f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../imagine/imagine',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

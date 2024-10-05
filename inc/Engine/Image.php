b6280e77186540113c426cac9c8cd860385344c5
badasswp <badasswpdev@gmail.com>
2024-10-05 09:45:25 +0100

    chore: set up Imagine instance

--- inc/Engine/Image.php
+++ inc/Engine/Image.php
@@ -15,6 +15,27 @@ use Imagine\Image\ImageInterface as Image_Object;
 
 class Image {
 	/**
+	 * Imagine object.
+	 *
+	 * This object is an instance of the
+	 * Imagine class.
+	 *
+	 * @since 1.0.5
+	 *
+	 * @var Imagine
+	 */
+	protected $imagine;
+
+	/**
+	 * Set up.
+	 *
+	 * @since 1.0.5
+	 */
+	public function __construct() {
+		$this->imagine = new Imagine();
+	}
+
+	/**
 	 * Get Image Resource.
 	 *
 	 * @since 1.0.1
@@ -23,7 +44,7 @@ class Image {
 	 */
 	public function get_image(): Image_Object {
 		try {
-			return ( new Imagine() )->open( Watermarker::$file );
+			return $this->imagine->open( Watermarker::$file );
 		} catch ( \Exception $e ) {
 			throw new \Exception(
 				sprintf(

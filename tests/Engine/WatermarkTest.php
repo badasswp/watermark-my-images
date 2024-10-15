ba93b2d1b1a47c5edc78be9fda510baa48501317
badasswp <badasswpdev@gmail.com>
2024-10-15 19:55:22 +0100

    test: add tests for Watermarker class

---
+++ tests/Engine/WatermarkTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace WatermarkMyImages\Tests\Engine;
+
+use Mockery;
+use Exception;
+use WP_Mock\Tools\TestCase;
+
+use WatermarkMyImages\Engine\Watermarker;
+
+/**
+ * @covers \WatermarkMyImages\Engine\Watermarker::__construct
+ * @covers \WatermarkMyImages\Engine\Watermarker::get_watermark_abs_path
+ */
+class WatermarkerTest extends TestCase {
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		Watermarker::$file = '/var/www/wp-content/uploads/2024/10/sample.png';
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_get_watermark_abs_path() {
+		$watermarker = Mockery::mock( Watermarker::class )->makePartial();
+		$watermarker->shouldAllowMockingProtectedMethods();
+
+		$abs_path = $watermarker->get_watermark_abs_path();
+
+		$this->assertSame( $abs_path, '/var/www/wp-content/uploads/2024/10/sample-watermark-my-images.jpg' );
+		$this->assertConditionsMet();
+	}
+}

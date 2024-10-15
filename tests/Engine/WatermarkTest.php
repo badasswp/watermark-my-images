aa936775fe1b5fa68d3a2ff3d8d9bb27ff5b9d47
badasswp <badasswpdev@gmail.com>
2024-10-15 20:02:52 +0100

    test: add new test for get_watermark_rel_path()

--- tests/Engine/WatermarkTest.php
+++ tests/Engine/WatermarkTest.php
@@ -48,4 +48,23 @@ class WatermarkerTest extends TestCase {
 		$this->assertSame( $rel_path, '' );
 		$this->assertConditionsMet();
 	}
+
+	public function test_get_watermark_rel_path_passes() {
+		$service = Mockery::mock( Service::class )->makePartial();
+		$service->shouldAllowMockingProtectedMethods();
+		$service->image_id = 1;
+
+		$watermarker = Mockery::mock( Watermarker::class )->makePartial();
+		$watermarker->shouldAllowMockingProtectedMethods();
+		$watermarker->service = $service;
+
+		\WP_Mock::userFunction( 'wp_get_attachment_url' )
+			->with( 1 )
+			->andReturn( 'https://example.com/wp-content/uploads/2024/10/sample.png' );
+
+		$rel_path = $watermarker->get_watermark_rel_path();
+
+		$this->assertSame( $rel_path, 'https://example.com/wp-content/uploads/2024/10/sample-watermark-my-images.jpg' );
+		$this->assertConditionsMet();
+	}
 }

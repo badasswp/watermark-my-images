6e518c41be426794e2ccb6bb61241b47324c9a98
badasswp <badasswpdev@gmail.com>
2024-10-21 07:31:21 +0100

    test: add test for watermark metadata

--- tests/Services/MetaDataTest.php
+++ tests/Services/MetaDataTest.php
@@ -12,6 +12,8 @@ use WatermarkMyImages\Abstracts\Service;
  * @covers \WatermarkMyImages\Services\MetaData::register
  */
 class MetaDataTest extends TestCase {
+	public MetaData $metadata;
+
 	public function setUp(): void {
 		\WP_Mock::setUp();
 
@@ -28,6 +30,25 @@ class MetaDataTest extends TestCase {
 		\WP_Mock::expectActionAdded( 'watermark_my_images_on_woo_product_get_image', [ $this->metadata, 'add_watermark_metadata' ], 10, 3 );
 
 		$this->metadata->register();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_add_watermark_metadata_fails_on_is_wp_error() {
+		$error = Mockery::mock( \WP_Error::class )->makePartial();
+		$error->shouldAllowMockingProtectedMethods();
+
+		\WP_Mock::userFunction(
+			'is_wp_error',
+			[
+				'times' => 1,
+				'return' => function( $error ) {
+					return $error instanceof \WP_Error;
+				}
+			]
+		);
+
+		$this->metadata->add_watermark_metadata( $error, [], 1 );
 
 		$this->assertConditionsMet();
 	}

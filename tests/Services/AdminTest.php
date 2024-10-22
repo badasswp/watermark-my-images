58460d47a1bc0a8ec158257722e2db8d77547e57
badasswp <badasswpdev@gmail.com>
2024-10-22 07:47:15 +0100

    test: register options menu passes

--- tests/Services/AdminTest.php
+++ tests/Services/AdminTest.php
@@ -4,12 +4,14 @@ namespace WatermarkMyImages\Tests\Services;
 
 use Mockery;
 use WP_Mock\Tools\TestCase;
+use WatermarkMyImages\Admin\Options;
 use WatermarkMyImages\Services\Admin;
 use WatermarkMyImages\Abstracts\Service;
 
 /**
  * @covers \WatermarkMyImages\Services\Admin::__construct
  * @covers \WatermarkMyImages\Services\Admin::register
+ * @covers \WatermarkMyImages\Services\Admin::register_options_menu
  * @covers \WatermarkMyImages\Engine\Watermarker::__construct
  */
 class AdminTest extends TestCase {
@@ -31,6 +33,53 @@ class AdminTest extends TestCase {
 		\WP_Mock::expectActionAdded( 'admin_enqueue_scripts', [ $this->admin, 'register_options_styles' ] );
 
 		$this->admin->register();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_options_menu() {
+		\WP_Mock::userFunction(
+			'esc_html__',
+			[
+				'times'  => 75,
+				'return' => function ( $text, $domain = 'watermark-my-images' ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr',
+			[
+				'times'  => 27,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction(
+			'esc_attr__',
+			[
+				'times'  => 18,
+				'return' => function ( $text ) {
+					return $text;
+				},
+			]
+		);
+
+		\WP_Mock::userFunction( 'add_menu_page' )
+			->with(
+				'Watermark My Images',
+				'Watermark My Images',
+				'manage_options',
+				'watermark-my-images',
+				[ $this->admin, 'register_options_page' ],
+				'dashicons-format-image',
+				100
+			);
+
+		$this->admin->register_options_menu();
 
 		$this->assertConditionsMet();
 	}

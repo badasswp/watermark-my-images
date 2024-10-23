c97a3d05aa34ae3b43821a20d863bf28e4fba4ab
badasswp <badasswpdev@gmail.com>
2024-10-23 15:49:51 +0100

    test: method bails out on image existence

--- tests/Services/WooCommerceTest.php
+++ tests/Services/WooCommerceTest.php
@@ -19,10 +19,14 @@ class WooCommerceTest extends TestCase {
 		\WP_Mock::setUp();
 
 		$this->woocommerce = new WooCommerce();
+
+		$this->create_mock_image( __DIR__ . '/sample.png' );
 	}
 
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
+
+		$this->destroy_mock_image( __DIR__ . '/sample.png' );
 	}
 
 	public function test_register() {
@@ -31,5 +35,51 @@ class WooCommerceTest extends TestCase {
 		$this->woocommerce->register();
 
 		$this->assertConditionsMet();
+	}
+
+	public function test_add_watermark_on_get_image_bails_on_image_existence() {
+		$product = Mockery::mock( \WC_Product::class )->makePartial();
+		$product->shouldAllowMockingProtectedMethods();
+
+		$product->shouldReceive( 'get_image_id' )
+			->andReturn( 1 );
+
+		\WP_Mock::userFunction( 'get_post_meta' )
+			->once()
+			->with( 1, 'watermark_my_images', true )
+			->andReturn(
+				[
+					'abs' => __DIR__ . '/sample.png',
+					'rel' => 'https://example.com/wp-content/uploads/2024/10/sample-1-watermark-my-images.jpg',
+				]
+			);
+
+		$image = $this->woocommerce->add_watermark_on_get_image(
+			'<img src="">',
+			$product,
+			'woocommerce_thumbnail',
+			[],
+			true
+		);
+
+		$this->assertConditionsMet();
+	}
+
+	public function create_mock_image( $image_file_name ) {
+		// Create a blank image.
+		$width  = 400;
+		$height = 200;
+		$image  = imagecreatetruecolor( $width, $height );
+
+		// Set background color.
+		$bg_color = imagecolorallocate( $image, 255, 255, 255 );
+		imagefill( $image, 0, 0, $bg_color );
+		imagejpeg( $image, $image_file_name );
+	}
+
+	public function destroy_mock_image( $image_file_name ) {
+		if ( file_exists( $image_file_name ) ) {
+			unlink( $image_file_name );
+		}
 	}
 }

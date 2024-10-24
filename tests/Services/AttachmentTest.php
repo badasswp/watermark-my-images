52288fa83dc09dc295b3a5f566c86c0f52a9c258
badasswp <badasswpdev@gmail.com>
2024-10-24 21:56:53 +0100

    test: register method passes

---
+++ tests/Services/AttachmentTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WatermarkMyImages\Tests\Services;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+use WatermarkMyImages\Abstracts\Service;
+use WatermarkMyImages\Services\Attachment;
+
+/**
+ * @covers \WatermarkMyImages\Services\Attachment::__construct
+ * @covers \WatermarkMyImages\Services\Attachment::register
+ * @covers \WatermarkMyImages\Engine\Watermarker::__construct
+ */
+class AttachmentTest extends TestCase {
+	public Attachment $attachment;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		$this->attachment = new Attachment();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	public function test_register() {
+		\WP_Mock::expectActionAdded( 'add_attachment', [ $this->attachment, 'add_watermark_on_add_attachment' ], 10, 1 );
+		\WP_Mock::expectActionAdded( 'delete_attachment', [ $this->attachment, 'remove_watermark_on_attachment_delete' ], 10, 1 );
+		\WP_Mock::expectFilterAdded( 'attachment_fields_to_edit', [ $this->attachment, 'add_watermark_attachment_fields' ], 10, 2 );
+		\WP_Mock::expectFilterAdded( 'wp_generate_attachment_metadata', [ $this->attachment, 'add_watermark_to_metadata' ], 10, 3 );
+		\WP_Mock::expectFilterAdded( 'wp_prepare_attachment_for_js', [ $this->attachment, 'show_watermark_images_on_wp_media_modal' ], 10, 3 );
+
+		$this->attachment->register();
+
+		$this->assertConditionsMet();
+	}
+}

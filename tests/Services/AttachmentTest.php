45e53cb5d66076dee66120df59e27c5fb024964b
badasswp <badasswpdev@gmail.com>
2024-11-01 03:26:17 +0100

    test: method bails out if no watermark post meta is present

--- tests/Services/AttachmentTest.php
+++ tests/Services/AttachmentTest.php
@@ -239,6 +239,24 @@ class AttachmentTest extends TestCase {
 		$this->assertConditionsMet();
 	}
 
+	public function test_remove_watermark_on_attachment_delete_bails_if_no_watermark_post_meta() {
+		\WP_Mock::userFunction( 'wp_attachment_is_image' )
+			->with( 1 )
+			->andReturn( true );
+
+		\WP_Mock::userFunction( 'get_post_meta' )
+			->with( 1, 'watermark_my_images', true )
+			->andReturn( '' );
+
+		\WP_Mock::userFunction( 'wp_get_attachment_metadata' )
+			->with( 1 )
+			->andReturn( [] );
+
+		$this->attachment->remove_watermark_on_attachment_delete( 1 );
+
+		$this->assertConditionsMet();
+	}
+
 	public function create_mock_image( $image_file_name ) {
 		// Create a blank image.
 		$width  = 400;

f9c5d1ddd5346517be7218b1063c8bc46c3c1b40
badasswp <badasswpdev@gmail.com>
2025-01-01 15:43:04 +0100

    test: add tests for get_form

--- tests/Admin/FormTest.php
+++ tests/Admin/FormTest.php
@@ -8,6 +8,7 @@ use WatermarkMyImages\Admin\Form;
 
 /**
  * @covers \WatermarkMyImages\Admin\Form::get_options
+ * @covers \WatermarkMyImages\Admin\Form::get_form
  */
 class FormTest extends TestCase {
 	public Form $form;
@@ -52,6 +53,34 @@ class FormTest extends TestCase {
 				'summary' => 'Plugin Summary',
 				'form'    => 'Plugin Form',
 			]
+		);
+	}
+
+	public function test_get_form() {
+		$form = Mockery::mock( Form::class )->makePartial();
+		$form->shouldAllowMockingProtectedMethods();
+
+		$form->shouldReceive( 'get_form_action' )
+			->andReturn( 'https://example.com' );
+
+		$form->shouldReceive( 'get_form_notice' )
+			->andReturn( 'Form Notice' );
+
+		$form->shouldReceive( 'get_form_main' )
+			->andReturn( 'Form Main' );
+
+		$form->shouldReceive( 'get_form_submit' )
+			->andReturn( 'Form Submit' );
+
+		$plugin_form = $form->get_form();
+
+		$this->assertSame(
+			'<form class="badasswp-form" method="POST" action="https://example.com">
+				Form Notice
+				<div class="badasswp-form-main">Form Main</div>
+				<div class="badasswp-form-submit">Form Submit</div>
+			</form>',
+			$plugin_form
 		);
 	}
 }

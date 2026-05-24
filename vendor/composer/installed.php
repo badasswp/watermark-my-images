ff466c1ecb0dd99e676b529286f435cd9b8e0b5c
badasswp <badasswpdev@gmail.com>
2026-05-24 23:39:12 +0000

    Build from release-1.2.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/watermark-my-images',
+        'pretty_version' => 'dev-release-1.2.0-built',
+        'version' => 'dev-release-1.2.0-built',
+        'reference' => '4159ea2de5eca8089e76c240287f741eb96fd49c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/watermark-my-images' => array(
+            'pretty_version' => 'dev-release-1.2.0-built',
+            'version' => 'dev-release-1.2.0-built',
+            'reference' => '4159ea2de5eca8089e76c240287f741eb96fd49c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'imagine/imagine' => array(
+            'pretty_version' => '1.5.2',
+            'version' => '1.5.2.0',
+            'reference' => 'f9ed796eefb77c2f0f2167e1d4e36bc2b5ed6b0c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../imagine/imagine',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
